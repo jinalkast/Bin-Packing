06f9e044335fcac97449f0fd3c27b52e7cf3ee95
Jinal <kasturij@mcmaster.ca>
2022-10-20 13:20:03 -0400

    completed online algos for t2

--- macpacking/algorithms/online.py
+++ macpacking/algorithms/online.py
@@ -24,3 +24,54 @@ class WorstSolution(Online):
         for w in stream:
             solution.append([w])
         return solution
+
+class FirstFit(Online):
+
+    def _process(self, capacity: int, stream: WeightStream) -> Solution:
+        solution = [[]]
+        for w in stream:
+            added = False
+            for bin in solution:
+                if sum(bin) + w <= capacity:
+                    bin.append(w)
+                    added = True
+                    break
+            if not added:
+                solution.append([w])
+        return solution
+
+class BestFit(Online):
+
+    def _process(self, capacity: int, stream: WeightStream) -> Solution:
+        solution = [[]]
+        for w in stream:
+            bin_index = -1
+            max_load = 0
+            for i in range(len(solution)):
+                if sum(solution[i]) + w <= capacity:
+                    if sum(solution[i]) >= max_load:
+                        bin_index = i
+                        max_load = sum(solution[i])
+            if bin_index >= 0:
+                solution[bin_index].append(w)    
+            else:
+                solution.append([w])
+        return solution
+
+class WorstFit(Online):
+
+    def _process(self, capacity: int, stream: WeightStream) -> Solution:
+        solution = [[]]
+        for w in stream:
+            bin_index = -1
+            min_load = capacity
+            for i in range(len(solution)):
+                if sum(solution[i]) + w <= capacity:
+                    if sum(solution[i]) <= min_load:
+                        bin_index = i
+                        min_load = sum(solution[i])
+            if bin_index >= 0:
+                solution[bin_index].append(w)    
+            else:
+                solution.append([w])
+        return solution
