e4bebb2dbaffb2fa5a53eb1a953155c715337f3f
Jinal <kasturij@mcmaster.ca>
2022-10-20 16:08:16 -0400

    updated offline algos for t2

--- macpacking/algorithms/offline.py
+++ macpacking/algorithms/offline.py
@@ -1,13 +1,20 @@
 from .. import Solution, WeightSet
 from ..model import Offline
-from .online import NextFit as Nf_online
+from .online import NextFit as Nf_online, FirstFit as Ff_online, BestFit as Bf_online, WorstFit as Wf_online
 
+class OfflineDecreasing(Offline):
+        def _process(self, capacity: int, weights: WeightSet) -> Solution:
+            weights = sorted(weights, reverse=True)
+            return self.__delegation((capacity, weights))
 
-class NextFit(Offline):
+class NextFit(OfflineDecreasing):
+    _OfflineDecreasing__delegation = Nf_online()
 
-    def _process(self, capacity: int, weights: WeightSet) -> Solution:
-        '''An offline version of NextFit, ordering the weigh stream and
-        delegating to the online version (avoiding code duplication)'''
-        weights = sorted(weights, reverse=True)
-        delegation = Nf_online()
-        return delegation((capacity, weights))
+class FirstFit(OfflineDecreasing):
+    _OfflineDecreasing__delegation = Ff_online()
+
+class BestFit(OfflineDecreasing):
+    _OfflineDecreasing__delegation = Bf_online()
+
+class WorstFit(OfflineDecreasing):
+    _OfflineDecreasing__delegation = Wf_online()
