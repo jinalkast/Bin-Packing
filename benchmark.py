f55f0fead39dba31e1905da77f8a6189844cdbfc
Maged Armanios <armanm5@mcmaster.ca>
2022-11-01 18:29:25 -0400

    Updated graphs in report to bar graph

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,9 @@
 import pyperf
 from os import listdir
 from os.path import isfile, join, basename
-from .macpacking.algorithms.offline import NextFitOff as NFOff, WorstFitOff as WFOff, BestFitOff as BFOff, FirstFitOff as FFOff
-from ..macpacking.algorithms.online import NextFitOn as NFOn, WorstSolution as WS, WorstFitOn as WFOn, BestFitOn as BFOn, FirstFitOn as FFOn
-from ..macpacking.reader import BinppReader
+from macpacking.algorithms.offline import NextFitOff as NFOff, WorstFitOff as WFOff, BestFitOff as BFOff, FirstFitOff as FFOff
+from macpacking.algorithms.online import NextFitOn as NFOn, WorstSolution as WS, WorstFitOn as WFOn, BestFitOn as BFOn, FirstFitOn as FFOn
+from macpacking.reader import BinppReader
 # We consider:
 #   - 50 objects (N1)
 #   - bin capacity of 150 (C3)
@@ -22,7 +22,7 @@ ONLINE_STRATEGIES = [
 def main():
     '''Example of benchmark code'''
     cases = list_case_files(CASES)
-    run_bench_runningTime(cases, ONLINE_STRATEGIES)
+    run_bench_runningTime([cases[0]], OFFLINE_STRATEGIES)
 
 
 def list_case_files(dir: str) -> list[str]:
